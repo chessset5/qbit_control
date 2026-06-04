76a3089fb84d680917ea00b9ca1ad032dc7d4583
Aaron Shackelford <grayjackel5@gmail.com>
2026-06-04 13:22:55 -0700

    cleaning up logic

--- src/logic/do_not_download.py
+++ src/logic/do_not_download.py
@@ -5,7 +5,6 @@ turns all hd-torrent files to do not download
 """
 
 from src.helpers.qbit import QBT_CLIENT
-from typing import cast
 
 
 def stop_tracking() -> None:
@@ -15,9 +14,6 @@ def stop_tracking() -> None:
 
     """
     with QBT_CLIENT as qc:
-        for tag in qc.torrents_tags():
-            if "HD" in tag:
-                print(tag)
-                for torrent in qc.torrents_info(tag=cast(str, tag)):
-                    ids: list[int] = [file.id for file in torrent.files]
-                    torrent.file_priority(file_ids=ids, priority=0)
+        for torrent in qc.torrents_info():
+            ids: list[int] = [file.id for file in torrent.files]
+            torrent.file_priority(file_ids=ids, priority=0)

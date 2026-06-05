d2ed26cf96bb7e421e24c7d76ba07c631f931cbd
Aaron Shackelford <grayjackel5@gmail.com>
2026-06-04 17:15:19 -0700

    adding new logic, for grabbing new data

---
+++ src/logic/get_tracker_data.py
@@ -0,0 +1,48 @@
+"""
+grabs the data from the qbit client
+
+"""
+
+from pathlib import Path
+from typing import Any
+import json
+
+from src import DATA_FOLDER
+from src.helpers.qbit import QBT_CLIENT
+
+
+def get_data() -> None:
+    """
+    Get user data
+    """
+
+    write_path: Path = DATA_FOLDER / "qbit" / "torrents_data.json"
+    write_path.parent.mkdir(exist_ok=True, parents=True)
+    data: list[dict[str, Any]] = []
+    with QBT_CLIENT as qc:
+        for torrent in qc.torrents.info():
+            add: bool = False
+            inner_data: dict = {}
+            inner_data["name"] = torrent.name
+            inner_data["hash"] = torrent.hash
+
+            files: list[dict[str, Any]] = []
+            for file in torrent.files:
+                if file.progress > 0 and file.progress < 1:
+                    add = True
+                files.append(
+                    {
+                        "availability": file.availability,
+                        "name": file.name,
+                        "size": file.size,
+                        "progress": file.progress,
+                        "id": file.id,
+                    }
+                )
+            files.sort(key=lambda x: x["name"])
+            inner_data["files"] = files
+            if add:
+                data.append(inner_data)
+    data.sort(key=lambda x: x["name"])
+    with open(file=write_path, mode="w", encoding="utf-8") as f:
+        json.dump(obj=data, fp=f, indent=4)
